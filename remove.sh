7a488f52f5d48624c97d8d4966dfcd59b8c60be9
FromDerik <DerikMalcolm@gmail.com>
2017-02-28 00:44:35 -0600

    FIXED BOIIIII

--- remove.sh
+++ remove.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-find debs -type f -name '*.deb' -delete
 rm -r Packages.bz2
--- update.sh
+++ update.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
-./remove.sh
-./packages.sh
-./push.sh
+rm -r Packages.bz2
+rm -r Packages
+dpkg-scanpackages ./debs > Packages
+bzip2 -fks Packages
+
+git add --all
+git commit -m "$*"
+git push
