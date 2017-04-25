5a753c34e2050e5cfedf1164f25188534df66dc8
the-casle <redeyedtreefrog28@gmail.com>
2017-04-25 12:35:52 -0500

    update

--- cydia/deb.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-dpkg-deb -bZgzip projects/searchbar debs
-dpkg-deb -bZgzip projects/Annoyance debs
-dpkg-deb -bZgzip projects/NotifyMe debs
-# dpkg-deb -bZgzip projects/<package name> <output folder>
