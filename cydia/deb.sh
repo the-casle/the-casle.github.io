64ee8d7af405144762d5f542801347a60612f932
FromDerik <DerikMalcolm@gmail.com>
2017-04-25 11:54:37 -0500

    Removed Dark iOS 10 i dont need two things to keep up with lmao

--- cydia/deb.sh
+++ cydia/deb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-dpkg-deb -bZgzip projects/DarkiOS10 debs
+dpkg-deb -bZgzip projects/searchbar debs
 dpkg-deb -bZgzip projects/Annoyance debs
 dpkg-deb -bZgzip projects/NotifyMe debs
 # dpkg-deb -bZgzip projects/<package name> <output folder>
