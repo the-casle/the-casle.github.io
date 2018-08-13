b825dfd5684b4316ffc3e3656549f3b4a91094c8
the casle <redeyedtreefrog28@gmail.com>
2018-08-12 22:29:44 -0500

    Cleaning

--- PackageSource/timemover/timemoverpreferences/NZ9RootListController.h
+++
@@ -1,5 +0,0 @@
-#import <Preferences/PSListController.h>
-
-@interface NZ9RootListController : PSListController
-
-@end
--- cydia/casle.sh
+++
@@ -1,8 +0,0 @@
-rm -r Packages*
-
-dpkg-scanpackages -m ./debs /dev/null > Packages
-bzip2 -fks Packages
-
-git add --all
-git commit -m "$*"
-git push
